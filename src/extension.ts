da0b7840dacb3827fafe800d1b2e363f8f18e17a
Matthias Glatthorn <mail@matthias-glatthorn.de>
2025-07-23 23:56:31 +0200

    feat: wip ai code detection

--- src/extension.ts
+++ src/extension.ts
@@ -9,15 +9,35 @@ export function activate(context: vscode.ExtensionContext) {
 	recorder = new KeyStrokeRecorder();
 	vscode.window.showInformationMessage("KSPT activate");
 
-  	vscode.commands.registerCommand('extension.logKey', (args) => {
+  disposable = vscode.commands.registerCommand('extension.logKey', (args) => {
 		recorder.onLogKey();
   	});
 
-	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
-		if (event.contentChanges.length > 0) {
-			vscode.commands.executeCommand('extension.logKey');
-		}
-	}));
+	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(async event => {
+		if (
+      event.document.fileName !== 'exthost' && // exclude logging
+      event.contentChanges.length > 0
+    ) {
+      const change = event.contentChanges[0];
+
+      // Handle deletion
+      if (change.text === '' && change.rangeLength > 0) {
+        return vscode.window.showInformationMessage("KSPT delete");
+      }
+
+      const changedText = change.text.replace(/(\r\n|\n|\r)/g, "");
+      if (changedText.length === 1) {
+        return vscode.commands.executeCommand('extension.logKey');
+      }
+
+      if (changedText.length > 1) {
+        const clipboardText = await vscode.env.clipboard.readText();
+        if (changedText !== clipboardText.replace(/(\r\n|\n|\r)/g, "")) {
+          return vscode.window.showInformationMessage("KSPT large change"); // Code was added by AI
+        }
+      }
+    }}
+	));
 
 	context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(event => {
 		recorder.saveStateToFile();
@@ -25,7 +45,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable);
 }
-
 
 export function deactivate() {
 	disposable.dispose();
